1fc7406059f21e35d8de46899927eb15fad73faa
Good name <1143943343@qq.com>
2022-12-19 21:21:36 +0800

    V1.7

--- LibraryManager/booksearch.h
+++ LibraryManager/booksearch.h
@@ -29,7 +29,7 @@ private:
     Ui::BookSearch *ui;
     DBOperator DBO;
     QSqlTableModel *tm;
-    QString Username = "Lee";
+    QString Username ;
     bool searchflag;
     QString bookName;
 };
